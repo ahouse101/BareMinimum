484ee0bdb02f697a63db6d73995c1c5352f01b6a
Alex House <ahouse101@gmail.com>
2013-08-28 22:39:07 +0100

    v0.3.4.0r1

--- BareMinimum/MainWin.Designer.cs
+++ BareMinimum/MainWin.Designer.cs
@@ -203,7 +203,7 @@
 			this.CalculationTypeComboBox.Items.AddRange(new object[] {
             "Even",
             "Most Zeros"});
-			this.CalculationTypeComboBox.Location = new System.Drawing.Point(339, 375);
+			this.CalculationTypeComboBox.Location = new System.Drawing.Point(338, 375);
 			this.CalculationTypeComboBox.Name = "CalculationTypeComboBox";
 			this.CalculationTypeComboBox.Size = new System.Drawing.Size(106, 23);
 			this.CalculationTypeComboBox.TabIndex = 8;
@@ -213,7 +213,7 @@
 			this.ScenarioTargetUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.ScenarioTargetUpDown.Enabled = false;
 			this.ScenarioTargetUpDown.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.ScenarioTargetUpDown.Location = new System.Drawing.Point(449, 375);
+			this.ScenarioTargetUpDown.Location = new System.Drawing.Point(448, 375);
 			this.ScenarioTargetUpDown.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -287,7 +287,7 @@
 			this.ScenarioTree.RowHeight = 25;
 			this.ScenarioTree.ShowGroups = false;
 			this.ScenarioTree.ShowImagesOnSubItems = true;
-			this.ScenarioTree.Size = new System.Drawing.Size(519, 355);
+			this.ScenarioTree.Size = new System.Drawing.Size(518, 355);
 			this.ScenarioTree.TabIndex = 3;
 			this.ScenarioTree.UseCellFormatEvents = true;
 			this.ScenarioTree.UseCompatibleStateImageBehavior = false;
@@ -334,6 +334,8 @@
 			this.ItemMarkedColumn.CheckBoxes = true;
 			this.ItemMarkedColumn.HeaderImageKey = "(none)";
 			this.ItemMarkedColumn.HeaderTextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.ItemMarkedColumn.MaximumWidth = 20;
+			this.ItemMarkedColumn.MinimumWidth = 20;
 			this.ItemMarkedColumn.Text = "";
 			this.ItemMarkedColumn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
 			this.ItemMarkedColumn.Width = 20;
